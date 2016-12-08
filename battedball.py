3471a458c1c55483d971245c988c7056cb4904fc
Patrick <patrick.wong@uwaterloo.ca>
2016-12-08 12:41:36 -0500

    added a stat dictionary

--- battedball.py
+++ battedball.py
@@ -1,13 +1,17 @@
 #import numpy
 import json
 
-# global dictionary
+# global dictionaries
+# pdict: conversion of json file to dictionary of players and their batted ball numbers
+# statdict: stores certain playerbase stats, i.e. number of players in pdict
+
 pdict = {}
+statdict = {}
 
 # merging list of free agents with dictionary
 # if player is a free agent, change their free agent status to True
 def merge_fas():
-    falist = open('shortfalist.txt')
+    falist = open('fullfalist.txt')
     for fa in falist:
         f_a = fa.strip('\r\n')
         if f_a in pdict:
@@ -19,18 +23,24 @@ def merge_fas():
 def parse_and_dict():
     # just change the filename to read the whole json
     # working on a smaller subset to save speed
-    json1_file = open('bbjson3.json')
+    json1_file = open('playerlist.json')
     json1_str = json1_file.read()
 
     # json.loads turns the json into a list of dictionaries
     json1_data = json.loads(json1_str)  # gets the whole dictionary
+    playercounter = 0
 
+    # populate the dictionary
     for player in json1_data:
         pname = player['name']
 
         # to int: avg_distance, avg_hr_distance, batter, max_distance, player_id
         player['avg_distance'] = int(player['avg_distance'])
-        player['avg_hr_distance'] = int(player['avg_hr_distance'])
+        ahd = str(player['avg_hr_distance'])                # manually changed null to "null" in list
+        if ahd.lower() == 'null':
+            player['avg_hr_distance'] = 0
+        else:
+            player['avg_hr_distance'] = int(player['avg_hr_distance'])
         player['batter'] = int(player['batter'])
         player['max_distance'] = int(player['max_distance'])
         player['player_id'] = int(player['player_id'])
@@ -50,10 +60,15 @@ def parse_and_dict():
         else:
             player['freeagent'] = False
 
-        pdict[pname] = player
-        pseason = str(player['season'])     # season is treated as an int
-        pmhs = player['max_hit_speed']
-        print "in " + pseason + ", " + pname + " had max hit speed of " + str(pmhs)
+        pdict[pname] = player       # sets a player's value in the dictionary
+        playercounter += 1
+        # debugging statements
+        # pseason = str(player['season'])     # season is treated as an int
+        # pmhs = player['max_hit_speed']
+        # print "in " + pseason + ", " + pname + " had max hit speed of " + str(pmhs)
+        # end loop
+    # more code
+    statdict['pc'] = playercounter
 # end parse_and_dict
 
 
@@ -63,24 +78,36 @@ def main():
     # will be useful when updating a player's FA stats
     # dictionary AO(1) speed to update, access
     # list is O(n) update, access
+
+    # populate pdict
     parse_and_dict()
 
     # to check if item is in dict, do this: ITEM in dict_name
-    gs1 = pdict['Giancarlo Stanton']
-    print str(gs1['name']) + " had an average speed of " + str(gs1['gb']) + " mph on his groundballs"
-    # merge_fas()
-    # for key in pdict:
+    gsname = "Giancarlo Stanton"
+    if gsname in pdict:
+        gs1 = pdict[gsname]
+        print str(gs1['name']) + " had an average speed of " + str(gs1['gb']) + " mph on his groundballs"
+    else:
+        print gsname + " isn't in the dictionary"
+    if 'Bob Sutton' in pdict:
+        print "how??"
+    else:
+        print "Bob Sutton not in the dictionary"
+
+    pcname = "pc"
+    if pcname in statdict:
+        print "there are " + str(statdict[pcname]) + " players recorded"
+    else:
+        print pcname + " isn't in the stat dictionary"
+
+    merge_fas()
+    # debugging statement
+    #for key in pdict:
     #    player = pdict[key]
     #    if player['freeagent']:
     #        print player['name'] + " is a free agent"
-    #    else:
-    #        print player['name'] + " is not a free agent"
-    merge_fas()
-    for key in pdict:
-        player = pdict[key]
-        if player['freeagent']:
-            print player['name'] + " is a free agent"
 
 # end main
 
+# run program
 main()
